efdf93f7bf210a94e368b6b55d1caaa6fd41d384
Chakib Assani <assanichakib@gmail.com>
2022-05-19 16:38:21 +0200

    product

--- templates/index.php
+++ templates/index.php
@@ -39,20 +39,15 @@
                   Nos produits
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Café en grain</a></li>
-                  <li><a class="dropdown-item" href="#">Café en poudre</a></li>
-                  <li><a class="dropdown-item">Café en soluble</a></li>
-                  <li><a class="dropdown-item" href="#">Café en dossette</a></li>
-                </ul>
+                  <li><a class="dropdown-item" href="/category">Café moulu</a></li>
+                  <li><a class="dropdown-item" href="/category">Café dosette</a></li>
+                  <li><a class="dropdown-item" href="/category">Café capsule</a></li>
+                  <li><a class="dropdown-item" href="/category">Café en grains</a></li>
               </li>
               <li class="nav-item">
                 <a class="nav-link "></a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
           </div>
         </div>
     </nav>
---
+++ templates/product.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>moka</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+
+
+
+    <section class="products pt-5">
+		<div class="container-md">
+			<div class="row">
+				<div class="col-lg-12 py-5">
+					<h2>Robusta</h2>
+					<div class="row">
+						<div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/finca-la joya-salvador.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Finca La Joya Salvador</h5>
+									<h5 class="card-title">Prix : 15 €</h5>
+									<a href="/panier" class="btn btn-light">Details du produit</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images25-dosettes-expresso-mexique-custepec.jpg/" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Dosettes Expresso - Mexique Custepec</h5>
+									<h5 class="card-title">Prix : 12 €</h5>
+									<a href="/detail" class="btn btn-light">Details du produit</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/vietnam_cafe.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Dosettes Elsass Café</h5>
+									<h5 class="card-title">Prix : 18 €</h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+							</div>
+                            <div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/elsass-cafe-bio.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Elsass-cafe-bio</h5>
+									<h5 class="card-title">Prix : 17 €</h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+                                <div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/Papouasie Nouvelle-Guignée Morobe.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">papouasie-nouvelle-guignee-morobe.jpg</h5>
+									<h5 class="card-title">Prix : 5€ €</h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+                           
+                                <div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/Zambie Kachipapa.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Zambie Kachipapa</h5>
+									<h5 class="card-title">Prix : 6€ </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+
+                                <div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/decafeine-etui-de-10-capsules-compatibles-nespress..." class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Décaféiné - Capsules compatibles Nespresso</h5>
+									<h5 class="card-title">Prix : 4€ </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+                            
+                            <div class="col-sm-6 col-lg-4">
+							<div class="card text-center">
+								<div class="card-image">
+									<img src="../images/colombie-supremo-etui-de-10-capsules-compatibles-n..." class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Colombie Supremo - Capsules compatibles Nespresso</h5>
+									<h5 class="card-title">Prix : 4€ </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div>
+                             
+                             
+                                <div class="col-sm-6 col-lg-4">
+							   <div class="card text-center">
+								<div class="card-image">
+									<img src="../images/guatemala-antigua-etui-de-10-capsules-compatibles-..." class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Guatemala Antigua - Capsules compatibles Nespresso</h5>
+									<h5 class="card-title">Prix : 4 € </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div> 
+                                
+                                <div class="col-sm-6 col-lg-4">
+							   <div class="card text-center">
+								<div class="card-image">
+									<img src="../images/espresso-capsules-etui-de-10.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Expresso - Capsules compatibles Nespresso</h5>
+									<h5 class="card-title">Prix : 4 € </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div> 
+                                
+                                <div class="col-sm-6 col-lg-4">
+							   <div class="card text-center">
+								<div class="card-image">
+									<img src="../images/colombie-supremo.jpg/" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Colombie Supremo</h5>
+									<h5 class="card-title">Prix : 5 € </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div> 
+                                
+                                <div class="col-sm-6 col-lg-4">
+							   <div class="card text-center">
+								<div class="card-image">
+									<img src="../images/degustation.jpg" class="card-img-top">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title">Dégustation</h5>
+									<h5 class="card-title">Prix : 6 € </h5>
+									<a href="/details" class="btn btn-light">Details du produit</a>
+								</div> 
+                                
+
+ 
+							</div>
+
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+
